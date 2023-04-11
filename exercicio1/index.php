c6e3ec71b4239f20478019982282a05ad8c112c2
ThaispFelix <felixthaisp@gmail.com>
2023-04-10 23:47:19 -0300

    aprendendo-php

---
+++ exercicio1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/exercicio1/style.css">
+        <title>Exercício 1</title>
+        
+</head>
+<body>
+    <h1>Exercício 1</h1>
+    <form action="/exercicio1" method="post">
+        <div>
+            <label for="value1"> Digite o primeiro valor </label>     
+            <input type="number" name="value1">
+        </div>
+        <div>
+            <label for="value2"> Digite o segundo valor </label>
+            <input type="number" name="value2">
+        </div>
+    <input type="submit" value="Enviar">
+    </form>
+<body>
+</html>   
