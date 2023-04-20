c7b56be1fb0772a904f46a6c9f5d15590a1e8e23
ThaispFelix <felixthaisp@gmail.com>
2023-04-19 23:35:20 -0300

    Primeiros-codigos-necessários-para-o-esqueleto-do-formulario

---
+++ exercicio3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/exercicio3/style.CSS">
+        <title>Exercício 3</title>
+        
+        
+        
+</head>
+<body>
+    <h1>Exercício 3</h1>
+    <h2>Formulário de Identificação Pessoal</h2>
+    <form action="/exercicio3/index.php" method="post">
+        
+        <div>
+            <label for="value1"> Nome: </label>     
+            <input type="text" name="value1">
+        </div>
+       
+
+        <div>
+            <label for="value2"> Orientação sexual: </label>     
+            <input type="text" name="value2">
+        </div>
+
+
+        <div>
+            <label for="value3"> Idade: </label>     
+            <input type="text" name="value3">
+        </div>
+
+
+    <input type="submit" value="Enviar">
+    </form>
