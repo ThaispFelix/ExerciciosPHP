dd61ce7610568434e9526dfdbea44698a475fb84
ThaispFelix <felixthaisp@gmail.com>
2023-05-17 00:55:42 -0300

    exercicio2-corrindo-identacao

--- exercicio2/index.php
+++ exercicio2/index.php
@@ -1,26 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/exercicio2/style.CSS">
         <title>Exercício 2</title>
-        
-        
-        
 </head>
 <body>
     <h1>Exercício 2</h1>
     <h3>Digite um número e saberá se o valor é divisível por 10, por 5, por 2 ou por nenhum desses</h3>
     <form action="/exercicio2" method="post">
-        
         <div>
             <label for="value1"> Digite o valor: </label>     
             <input type="text" name="value1">
         </div>
-       
-    <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar">
     </form>
-<body>
+</body>
 </html>   
