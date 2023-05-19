e95676cb776539b96e541d06f9a6e9f962a90120
ThaispFelix <felixthaisp@gmail.com>
2023-05-19 17:48:03 -0300

    exercicio 5: alinhando html com head

--- exercicio5/index.php
+++ exercicio5/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/exercicio5/style.CSS">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio5/style.CSS">
         <title>Exercício 5</title>
               
 </head>
