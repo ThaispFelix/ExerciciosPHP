39b3d821ce258b6c3d7c2c4497a426e5f45f9a67
ThaispFelix <felixthaisp@gmail.com>
2023-05-20 17:52:17 -0300

    Exercicio 3: acertando identação, espaços em brancos e variáveis do front

--- exercicio3/index.php
+++ exercicio3/index.php
@@ -1,37 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/exercicio3/style.CSS">
         <title>Exercício 3</title>
-        
-        
-        
+          
 </head>
 <body>
     <h1>Exercício 3</h1>
     <h2>Formulário de Identificação Pessoal</h2>
-    <form action="/exercicio3/index.php" method="post">
-        
+    <form action="/exercicio3/index.php" method="post">   
         <div>
-            <label for="value1"> Nome: </label>     
-            <input type="text" name="value1">
+            <label for="nome"> Nome: </label>     
+            <input type="text" name="nome">
         </div>
-       
-
         <div>
-            <label for="value2"> Orientação sexual: </label>     
-            <input type="text" name="value2">
+            <label for="genero"> Gênero: </label>     
+            <input type="text" name="genero">
         </div>
-
-
         <div>
-            <label for="value3"> Idade: </label>     
-            <input type="text" name="value3">
+            <label for="idade"> Idade: </label>     
+            <input type="text" name="idade">
         </div>
-
-
     <input type="submit" value="Enviar">
     </form>
+</body>
+</html>    
