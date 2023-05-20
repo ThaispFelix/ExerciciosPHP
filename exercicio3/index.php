8a35d664bba3759d4e2824c5be44d73c2c38b0d5
ThaispFelix <felixthaisp@gmail.com>
2023-05-20 17:47:13 -0300

    Exericio 3: tentando resolver erros nas condições do php

--- exercicio3/index.php
+++ exercicio3/index.php
@@ -1,50 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/exercicio3/style.CSS">
         <title>Exercício 3</title>
-        
-        
-        
+          
 </head>
 <body>
     <h1>Exercício 3</h1>
     <h2>Formulário de Identificação Pessoal</h2>
     <form action="/exercicio3/index.php" method="post">
-        
         <div>
-            <label for="value1"> Nome: </label>     
-            <input type="text" name="value1">
+            <label for="nome"> Nome: </label>     
+            <input type="text" name="nome">
+        </div>    
+        <div>
+            <label for="genero"> Gênero: </label>     
+            <input type="text" name="genero">
         </div>
-       
-
         <div>
-            <label for="value2"> Gênero: </label>     
-            <input type="text" name="value2">
-        </div>
-
-
-        <div>
-            <label for="value3"> Idade: </label>     
-            <input type="text" name="value3">
+            <label for="idade"> Idade: </label>     
+            <input type="text" name="idade">
         </div>
 
-        <?php
-     $value2 = $_POST ['value2'];{
-     $value3 = $_POST ['value3'];         
-         if ($value2 == "feminino" && $value3 < 25); {
+    <?php
+    if (array_key_exists ('genero',$_POST) ){
+     $genero = $_POST ['genero'];{
+     $idade = $_POST ['idade'];         
+        if ($genero == "feminino" && $idade < 25) {
            echo "<h3> Aceita </h3>"; 
         }
-        elseif ($value2 == "masculino") {
+        elseif ($genero == "masculino") {
            echo "<h3> Não aceito </h3>";    
         } 
-        elseif ($value2 == "não binário") {
+        elseif ($genero == "não binário") {
            echo "<h3> Não aceito </h3>";
+        }
+        elseif ($genero == "feminino" && $idade > 25) {
+            echo "<h3> Não aceita </h3>";
+        }
+        elseif ($genero == "feminino" && $idade == 25) {
+            echo "<h3> Aceita </h3>";
+        }
+        elseif ($genero == "feminino") {
+            echo "<h3> Falta preencher o campo idade </h3>";
+        }
         }
     }   
     ?>
     <input type="submit" value="Enviar">
     </form>
+</body>
+</html>    
