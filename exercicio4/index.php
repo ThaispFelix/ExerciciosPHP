2865606397dc09bf7503083b80cf96b87388bfca
ThaispFelix <felixthaisp@gmail.com>
2023-05-23 13:56:38 -0300

    Exercicio 4: corrigindo identação, body e html

--- exercicio4/index.php
+++ exercicio4/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/exercicio4/style.CSS">
-        <title>Exercício 4</title>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio4/style.CSS">
+    <title>Exercício 4</title>
                
 </head>
 <body>
@@ -27,3 +27,5 @@
         </div>
     <input type="submit" value="Enviar">
     </form>
+</body>
+</html>    
