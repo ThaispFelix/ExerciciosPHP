d7901ef9e96e25aa3134d2ed999049835a0de3c8
ThaispFelix <felixthaisp@gmail.com>
2023-05-23 22:06:15 -0300

    Exercicio 2: identificando conflitos

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # exercício de lógica para o projeto novos titans
+
+## Como rodar
+
+### Requisitos
+- PHP 8 ou superior
+
+#### Passo a passo
+1. Acesse a pasta do projeto
+2. Rode o comando 'php -S localhost:8585'
+3. Acesse no seu navegador a url 'localhost-8585'
+4. No indice, escolha o exercício que deseja testar
--- exercicio2/index.php
+++ exercicio2/index.php
@@ -1,21 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+    <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width-device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/exercicio2/style.CSS">
         <title>Exercício 2</title>
+        
+        
+        
 </head>
 <body>
     <h1>Exercício 2</h1>
     <h3>Digite um número e saberá se o valor é divisível por 10, por 5, por 2 ou por nenhum desses</h3>
     <form action="/exercicio2/index.php" method="post">
         <div>
-            <label for="value1"> Digite o valor</label>     
-            <input type="text" name="value1">
+            <label for="Divisor"> Digite o valor: </label>     
+            <input type="text" name="Divisor">
         </div>
-        <input type="submit" value="Enviar">
+       
+    <input type="submit" Divisor="Enviar">
     </form>
-</body>
+
+   <?php
+    if(isset ($_POST['Divisor'])){
+           $Divisor = $_POST['Divisor'];
+           if (($Divisor % 10 == 0) && ($Divisor % 5 == 0) && ($Divisor % 2 == 0)) {
+            echo "<h3> É divisível por 2, por 5 e por 10 </h3>";
+           }
+           elseif ($Divisor % 5 == 0) { 
+            echo "<h3>É divisível por 5</h3>";
+           } 
+           elseif ($Divisor % 2 == 0) { 
+            echo "<h3>É divisível por 2</h3>";
+           } 
+           elseif ($Divisor) {
+            echo "<h3>Não é divisível por 10, 5 ou 2</h3>";
+           }
+           elseif  ($Divisor % 10 == 0) {
+            echo "<h3>É divisivel por 10</h3>";
+           }
+    } 
+    ?>
+<body>
 </html>   
