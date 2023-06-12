3e93bd570c76e6d9e0ff7aa69852a56943574752
ThaispFelix <felixthaisp@gmail.com>
2023-06-12 19:16:14 -0300

    Exercicio 3- frontend: caixa de seleção para a variável gênero

--- exercicio3/index.php
+++ exercicio3/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/exercicio3/style.CSS">
-        <title>Exercício 3</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio3/style.CSS">
+    <title>Exercício 3</title>
           
 </head>
 <body>
@@ -18,7 +18,11 @@
         </div>
         <div>
             <label for="genero"> Gênero: </label>     
-            <input type="text" name="genero">
+                <select>
+                    <option> Feminino </option>
+                    <option> Masculino </option>
+                    <option> Não binário </option>
+                </select>
         </div>
         <div>
             <label for="idade"> Idade: </label>     
