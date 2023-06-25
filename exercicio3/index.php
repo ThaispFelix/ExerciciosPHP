0c7c7239022ab113efe168e0b64635f22eeb784f
ThaispFelix <felixthaisp@gmail.com>
2023-06-25 20:55:58 -0300

    Exercicio 3: acertando o front no back

--- exercicio3/index.php
+++ exercicio3/index.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/exercicio3/style.CSS">
-        <title>Exercício 3</title>
-          
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio3/style.CSS">
+    <title>Exercício 3</title>          
 </head>
 <body>
-    <h1>Exercício 3</h1>
-    <h2>Formulário de Identificação Pessoal</h2>
+    <h1 class = "titilo fonte-grande">Exercício 3</h1>
+    <h2  class = "sublinhado">Formulário de Identificação Pessoal</h2>
     <form action="/exercicio3/index.php" method="post">
         <div>
             <label for="nome"> Nome: </label>     
@@ -18,7 +17,11 @@
         </div>    
         <div>
             <label for="genero"> Gênero: </label>     
-            <input type="text" name="genero">
+            <select>
+                    <option> Feminino </option>
+                    <option> Masculino </option>
+                    <option> Não binário </option>
+                </select>
         </div>
         <div>
             <label for="idade"> Idade: </label>     
