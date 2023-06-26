7d0e3e7ac77faaf6ae1a91e8d568575fcb1cc25a
ThaispFelix <felixthaisp@gmail.com>
2023-06-26 13:34:27 -0300

    Exercicio 6: conserto na identação do css

--- exercicio6/index.php
+++ exercicio6/index.php
@@ -17,7 +17,7 @@
         <br><input class="btn" type="submit" name="btnCalcular" value="Mês Correspondente:"/></br> 
     </form>
     <?php
-	 if (array_key_exists('Mês',$_POST) ){
+	 if (array_key_exists('Mês',$_POST)){
 		$variavel = $_POST["Mês"];
 		switch ($variavel) {
 			  case 1:
