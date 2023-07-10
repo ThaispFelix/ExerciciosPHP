4e736dc8dd1d1d274bdca53d18db4a13e99e31f1
ThaispFelix <felixthaisp@gmail.com>
2023-07-09 22:00:19 -0300

    Exercicio 4: front corrigido

--- exercicio4corrigido/index.php
+++ exercicio4corrigido/index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width-device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/exercicio4/style.CSS">
+    <link rel="stylesheet" href="/exercicio4corrigido/style.CSS">
     <title>Exercício 4</title>              
 </head>
 <body>
     <h1 class= "titulo">Exercício 4</h1>
-    <h2 class= "subtitulo">Colocando números em ordem decrescente</h2>
-    <form action="/exercicio4/index.php" method="post">
+    <h2 class= "subtitulo">Colocando números em ordem decrescente:</h2>
+    <form action="/exercicio4corrigido/index.php" method="post">
         <div>
             <label for="value1"><h3> Primeiro número: </h3></label>     
             <input type="number" name="value1">
