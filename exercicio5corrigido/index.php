dfd892fc3adb7d6cb4332e939f1c77a0ef44e8d8
ThaispFelix <felixthaisp@gmail.com>
2023-07-21 16:52:34 -0300

    Exercicio 5: frontend sem conflitos

--- exercicio5corrigido/index.php
+++ exercicio5corrigido/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/exercicio5/style.CSS">
-        <title>Exercício 5</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio5/style.CSS">
+    <title>Exercício 5</title>
               
 </head>
 <body>
@@ -18,31 +18,13 @@
         </div>
         <div>
             <label for="value2"> Digite o segundo valor: </label>     
-            <input type="numb" name="value2">
+            <input type="numb" name="value3">
         </div>
         <div>
             <label for="value3"> Digite o terceiro valor: </label>     
             <input type="numb" name="value3">
         </div>
-        <input type="submit" value="Testar">
+        <input type="submit" value="Enviar">
     </form>
-    <?php
-    if (array_key_exists('value1',$_POST) ){
-        $a = $_POST['value1'];
-        $b = $_POST['value2'];
-        $c = $_POST['value3'];
-
-        if( ($a+$b>$c)&&($a+$c>$b)&&($b+$c>$a) ){
-            if($a==$b && $b==$c)
-                echo "Triângulo equilátero";
-            elseif ($a==$b || $a==$c || $b==$c)
-                echo "Triângulo isósceles";
-            else
-                echo "Triângulo escaleno";
-        }
-            else
-                echo "O triângulo não existe";
-    }
-    ?>
 </body>
 </html> 
