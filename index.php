c262dcd434eb2b9ffbbf7c0abb316fdc39577011
ThaispFelix <felixthaisp@gmail.com>
2023-07-25 11:56:07 -0300

    Índice atualizado

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width-device-width, initial-scale=1.0">
-        <title>Indice</title>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>Índice</title>
         
 </head>
 <body>
-  <h1>Indice </h1>
+  <h1>Índice </h1>
   <a href="/exercicio1">Exercicio 1</a>
-<body>
+  <a href="/exercicio2">Exercicio 2</a>
+  <a href="/exercicio3">Exercicio 3</a>
+  <a href="/exercicio4corrigido">Exercicio 4</a>
+  <a href="/exercicio5corrigido">Exercicio 5</a>
+  <a href="/exercicio6">Exercicio 6</a>
+</body>
 </html>   
