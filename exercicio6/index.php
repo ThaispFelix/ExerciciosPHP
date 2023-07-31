25aa312b1e6e903b9e510fb2b278fbc95ee240e7
ThaispFelix <felixthaisp@gmail.com>
2023-07-31 10:19:52 -0300

    Exercício 6: pequenas modificações do index e CSS pra resolver conflitos

--- exercicio6/index.php
+++ exercicio6/index.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <h1>Exercício 6</h1>
-    <h3>Digite um número e o mês correspondente será mostrado</h3>
+    <h3>Digite um número e o mês correspondente será mostrado:</h3>
     <form action="/exercicio6/index.php" method="POST">
             <label>Digite um número de 1 a 12:</label>
             <input type="text" name="Mês"/>
