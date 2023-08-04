81547f16d97e409e29f12e1d34d14741a40bee8a
ThaispFelix <felixthaisp@gmail.com>
2023-08-04 09:51:41 -0300

    Índice com readme no main

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 </head>
 <body>
   <h1>Índice </h1>
+  <a href="/README">README</a>
   <a href="/exercicio1">Exercicio 1</a>
   <a href="/exercicio2">Exercicio 2</a>
   <a href="/exercicio3">Exercicio 3</a>
   <a href="/exercicio4corrigido">Exercicio 4</a>
   <a href="/exercicio5corrigido">Exercicio 5</a>
   <a href="/exercicio6">Exercicio 6</a>
-  <a href="/exercicio6/readme">Readme</a>
   <a href="/exercicio7">Exercicio 7</a> 
 </body>
 </html>   
