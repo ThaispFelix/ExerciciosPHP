2d4524e68c6bdc8981c868a8e61ca88b445fea8f
ThaispFelix <felixthaisp@gmail.com>
2023-08-18 17:03:44 -0300

    Teste automatizado do front do exercício 1.

--- tests/Acceptance/Exercicio1Cest.php
+++ tests/Acceptance/Exercicio1Cest.php
@@ -12,7 +12,46 @@ class Exercício6Cest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function IndiceExercicio1Test(AcceptanceTester $I)
     {
+        $I->amOnPage("/");
+        $I->see("Exercício 1");
+        $I->click("Exercício 1");
+        $I->see("Exercicio 1");
+        $I->seeInTitle("Exercício 1");
     }
+
+    // tests
+    public function TitleExercicio1Test(AcceptanceTester $I)
+    {
+        $I->amOnPage("/exercicio1");
+        $I->seeInTitle("Exercício 1");
+        $I->see("Exercicio 1", "h1");
+    }
+
+    // tests
+    public function FormExercicio1Test(AcceptanceTester $I)
+    {
+        $I->amOnPage("/exercicio1");
+        $I->see("Digite o primeiro valor:");
+        $I->fillField("value1", "11");
+        $I->see("Digite o segundo valor:");
+        $I->fillField("value1", "10");
+        $I->click("Enviar");
+
+        $I->amOnPage("/exercicio1");
+        $I->see("Digite o primeiro valor:");
+        $I->fillField("value1", "10");
+        $I->see("Digite o segundo valor:");
+        $I->fillField("value1", "10");
+        $I->click("Enviar");
+
+        $I->amOnPage("/exercicio1");
+        $I->see("Digite o primeiro valor:");
+        $I->fillField("value1", "10");
+        $I->see("Digite o segundo valor:");
+        $I->fillField("value1", "9");
+        $I->click("Enviar");
+    }
+
 }
