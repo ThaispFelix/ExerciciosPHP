05d1a24e7b37863887aeaab704a35281da878b8a
ThaispFelix <felixthaisp@gmail.com>
2023-08-24 19:09:35 -0300

    Adicionar HTML do exercício 9.

---
+++ exercicio9/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio9/style.CSS">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <h1>Exercício 9</h1>
+    <h2>Quantas vezes deseja imprimir a palavra SOL?</h2>
+    <form action="/exercicio9/index.php" method="POST">
+        <div>
+            <label for= "numero"> Digite a quantidade de vezes que SOL deve se repetir: </label>
+            <input type= "numb" name= "numero"> 
+        </div>     
+        <input type= "submit" value="Imprimir">
+    </form>
+</body>
+</html>
