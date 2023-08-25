862d0d3aef5557b5e2c715953c8e8ad561f5423a
ThaispFelix <felixthaisp@gmail.com>
2023-08-24 22:03:21 -0300

    Teste automatizado do frontend do exercício 9.

---
+++ tests/Acceptance/Exercicio9Cest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function IndiceExercicio9Test(AcceptanceTester $I)
+    {
+        $I->amOnPage("/");
+        $I->see('Exercicio 9');
+        $I->click("Exercicio 9");
+        $I->seeInTitle("Exercício 9");
+    }
+
+    // tests
+    public function TitleExercicio9Test(AcceptanceTester $I)
+    {
+        $I->amOnPage("/exercicio9");
+        $I->seeInTitle("Exercício 9");
+        $I->see('Exercício 9', 'h1');
+    }
+
+    // tests
+    public function FormExercicio9Test(AcceptanceTester $I)
+    {
+        $I->amOnPage("/exercicio9");
+        $I->see("Exercício 9");
+        $I->see("Quantas vezes deseja imprimir a palavra SOL?");
+        $I->see("Digite a quantidade de vezes que SOL deve se repetir:");
+        $I->fillFiel("numero", "12");
+        $I->click("Enviar");
+    }
+}
