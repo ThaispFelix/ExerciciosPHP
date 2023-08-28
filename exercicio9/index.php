60e039694138d73e20c725c7277fed97266914b8
ThaispFelix <felixthaisp@gmail.com>
2023-08-28 00:43:36 -0300

    Adicionar index.php do exercício 9 com modificações do estilo.

--- exercicio9/index.php
+++ exercicio9/index.php
@@ -8,14 +8,14 @@
     <title>Exercício 9</title>
 </head>
 <body>
-    <h1>Exercício 9</h1>
-    <h2>Quantas vezes deseja imprimir a palavra SOL?</h2>
-    <form action="/exercicio9/index.php" method="POST">
+    <h1 class="cabeçalho">Exercício 9</h1>
+    <h2 class="cabeçalho">Quantas vezes deseja imprimir a palavra SOL?</h2>
+    <form action="/exercicio9/index.php" method="POST" class="form" class="login-page">
         <div>
             <label for= "numero"> Digite a quantidade de vezes que SOL deve se repetir: </label>
-            <input type= "numb" name= "numero"> 
+            <input type= "number" name= "numero"> 
         </div>     
-        <input type= "submit" value="Imprimir">
+        <input type= "submit" value="Imprimir" class="botao-input">
     </form>
 </body>
 </html>
