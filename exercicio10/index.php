dbe93197259839c860b5f779ee89d68c1e22cf5c
ThaispFelix <felixthaisp@gmail.com>
2023-09-01 18:55:56 -0300

    Adicionar form do exercicio 10.

---
+++ exercicio10/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio10/style.CSS">
+    <title>Exercício 10</title>
+</head>
+<body>
+    <h1>Exercício 10</h1>
+    <h2>Digite 20 números e saiba a soma dos positivos e o total de números negativos:</h2>
+    <form action="/exercicio10/index.php" method="POST">
+        <div>
+            <label for="value1"></label>
+            <input type= "number" name="value1">
+        </div>
+        <div>
+            <label for="value2"></label>     
+            <input type="number" name="value2">
+        </div>
+        <div>
+            <label for="value3"></label>     
+            <input type="number" name="value3">
+        </div>
+        <div>
+            <label for="value4"></label>     
+            <input type="number" name="value4">
+        </div>
+        <div>
+            <label for="value5"></label>     
+            <input type="number" name="value5">
+        </div>
+        <div>
+            <label for="value6"></label>     
+            <input type="number" name="value6">
+        </div>
+        <div>
+            <label for="value7"></label>     
+            <input type="number" name="value7">
+        </div>
+        <div>
+            <label for="value8"></label>     
+            <input type="number" name="value8">
+        </div>
+        <div>
+            <label for="value9"></label>     
+            <input type="number" name="value9">
+        </div>
+        <div>
+            <label for="value10"></label>     
+            <input type="number" name="value10">
+        </div>
+        <div>
+            <label for="value11"></label>     
+            <input type="number" name="value11">
+        </div>
+        <div>
+            <label for="value12"></label>     
+            <input type="number" name="value12">
+        </div>
+        <div>
+            <label for="value13"></label>     
+            <input type="number" name="value13">
+        </div>
+        <div>
+            <label for="value14"></label>     
+            <input type="number" name="value14">
+        </div>
+        <div>
+            <label for="value15"></label>     
+            <input type="number" name="value15">
+        </div>
+        <div>
+            <label for="value16"></label>     
+            <input type="number" name="value16">
+        </div>
+        <div>
+            <label for="value17"></label>     
+            <input type="number" name="value17">
+        </div>
+        <div>
+            <label for="value18"></label>     
+            <input type="number" name="value18">
+        </div>
+        <div>
+            <label for="value19"></label>     
+            <input type="number" name="value19">
+        </div>
+        <div>
+            <label for="value20"></label>     
+            <input type="number" name="value20">
+        </div>
+        <input type= "submit" value="Consultar">     
+    </form>
+</body>
+</html>
