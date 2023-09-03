5791294902c877e7121303443e3b87925f759b58
ThaispFelix <felixthaisp@gmail.com>
2023-09-02 21:12:21 -0300

    Adicionar teste automatizado do front do exercício 6 que foi apagado em outra branch.

--- tests/Acceptance/Exercicio6Cest.php
+++ tests/Acceptance/Exercicio6Cest.php
@@ -31,73 +31,70 @@ class Exercício6Cest
     // tests
     public function FormExercicio6Test(AcceptanceTester $I)
     {
-        // Eu estou na pagina "exercicio6"
+        
         $I->amOnPage("/exercicio6");
-        // Eu vejo "Digite um número de 1 a 12:"
         $I->see("Digite um número de 1 a 12:");
-        // Eu digito 1 no campo Mês
         $I->fillField("Mês","1");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 2 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","2");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 3 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","3");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 4 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","4");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 5 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","5");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 6 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","6");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 7 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","7");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 8 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","8");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 9 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","9");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 10 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","10");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
     
-        // Eu digito 11 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","11");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
 
-        // Eu digito 12 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","12");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
 
-        // Eu digito 13 no campo Mês
+        $I->amOnPage("/exercicio6");
+        $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","13");
-        // Eu clico em Mês Correspondente
         $I->click("Mês Correspondente:");
 
     } 
