0dbe3d53ad14fdd43366330bf64077730a0e13dc
ThaispFelix <felixthaisp@gmail.com>
2023-09-03 16:04:05 -0300

    Adicionar CSS ao formulário.

--- exercicio10/index.php
+++ exercicio10/index.php
@@ -4,94 +4,94 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width-device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/exercicio10/style.CSS">
+    <link rel="stylesheet" href="/exercicio10/style.css">
     <title>Exercício 10</title>
 </head>
 <body>
-    <h1>Exercício 10</h1>
-    <h2>Digite 20 números e saiba a soma dos positivos e o total de números negativos:</h2>
-    <form action="/exercicio10/index.php" method="POST">
+    <form action="/exercicio10/index.php" method="POST" class="form" class="login-page">
+    <h1 class="titulo">Exercício 10</h1>
+    <h2 class="sub-titulo">Digite 20 números e saiba a soma dos positivos e o total de números negativos:</h2>
         <div>
             <label for="value1"></label>
-            <input type= "number" name="value1">
+            <input type= "number" name="value1" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value2"></label>     
-            <input type="number" name="value2">
+            <input type="number" name="value2" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value3"></label>     
-            <input type="number" name="value3">
+            <input type="number" name="value3" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value4"></label>     
-            <input type="number" name="value4">
+            <input type="number" name="value4" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value5"></label>     
-            <input type="number" name="value5">
+            <input type="number" name="value5" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value6"></label>     
-            <input type="number" name="value6">
+            <input type="number" name="value6" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value7"></label>     
-            <input type="number" name="value7">
+            <input type="number" name="value7" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value8"></label>     
-            <input type="number" name="value8">
+            <input type="number" name="value8" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value9"></label>     
-            <input type="number" name="value9">
+            <input type="number" name="value9" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value10"></label>     
-            <input type="number" name="value10">
+            <input type="number" name="value10" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value11"></label>     
-            <input type="number" name="value11">
+            <input type="number" name="value11" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value12"></label>     
-            <input type="number" name="value12">
+            <input type="number" name="value12" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value13"></label>     
-            <input type="number" name="value13">
+            <input type="number" name="value13" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value14"></label>     
-            <input type="number" name="value14">
+            <input type="number" name="value14" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value15"></label>     
-            <input type="number" name="value15">
+            <input type="number" name="value15" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value16"></label>     
-            <input type="number" name="value16">
+            <input type="number" name="value16" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value17"></label>     
-            <input type="number" name="value17">
+            <input type="number" name="value17" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value18"></label>     
-            <input type="number" name="value18">
+            <input type="number" name="value18" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value19"></label>     
-            <input type="number" name="value19">
+            <input type="number" name="value19" placeholder= "Digite um número...">
         </div>
         <div>
             <label for="value20"></label>     
-            <input type="number" name="value20">
+            <input type="number" name="value20" placeholder= "Digite um número...">
         </div>
-        <input type= "submit" value="Consultar">     
+        <input type= "submit" value="Consultar" class="botao-submit">     
     </form>
 </body>
 </html>
