c215b33606dfd9e75555cb8a4354c5da632dc546
ThaispFelix <felixthaisp@gmail.com>
2023-09-02 21:23:05 -0300

    Adicionar o teste automatizado do back do exercício 6 que foi apagado do PR original.

--- tests/Acceptance/Exercicio6Cest.php
+++ tests/Acceptance/Exercicio6Cest.php
@@ -37,66 +37,79 @@ class Exercício6Cest
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","1");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Janeiro");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","2");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Fevereiro");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","3");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Março");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","4");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Abril");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","5");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Maio");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","6");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Junho");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","7");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Julho");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","8");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Agosto");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","9");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Setembro");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","10");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Outubro");
     
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","11");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Novembro");
 
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","12");
         $I->click("Mês Correspondente:");
+        $I->see("O Mês é Dezembro");
 
         $I->amOnPage("/exercicio6");
         $I->see("Digite um número de 1 a 12:");
         $I->fillField("Mês","13");
         $I->click("Mês Correspondente:");
+        $I->see("Não existe mês correspondente");
 
     } 
 }
